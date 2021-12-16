612a281b7a17936ad0366ce10d2f2f0c2d19467e
Marcelo Gobetti <mwgobetti@gmail.com>
2021-12-16 00:09:00 -0300

    Initial commit

---
+++ Shared/Xcode13_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode13_2App.swift
+//  Shared
+//
+//  Created by Marcelo Gobetti on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Xcode13_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
