4871bbc2afb55fc9dcecadcd9fb6a7c2a517858b
Marcelo Gobetti <mwgobetti@gmail.com>
2021-12-16 01:13:44 -0300

    Rename ContentView => CharactersView

--- Shared/CharactersView.swift
+++ Shared/CharactersView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CharactersView.swift
 //  Shared
 //
 //  Created by Marcelo Gobetti on 16/12/21.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CharactersView: View {
     @State private var characters: [Character]
     private let service = MarvelService()
 
@@ -30,7 +30,7 @@ extension Character: Identifiable {}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(characters: [
+        CharactersView(characters: [
             .init(id: 1, name: "Name", comics: .init(items: [
                 .init(resourceURI: nil)
             ]))
